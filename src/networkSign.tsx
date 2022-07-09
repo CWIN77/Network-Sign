c419d593dddc82c0f3c6c3212f654b4c579dff3f
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-07-09 16:57:33 +0900

    Update networkSign.tsx

--- src/networkSign.tsx
+++ src/networkSign.tsx
@@ -1,28 +1,27 @@
 import { useEffect, useState } from 'react'
-import styled from 'styled-components' 
+import styled from 'styled-components'
 
 const NetworkSign = () => {
   const [isOnline,setIsOnline] = useState(true);
-  useEffect(()=>{
+  const showSign = (bool:boolean)=> {
     const container = document.getElementById(Container.styledComponentId);
-    window.addEventListener('online',()=>{
-      setIsOnline(true);
-      if(container !== null){
+    setIsOnline(bool);
+    if(container !== null){
+      container.style.display = "flex";
+      setTimeout(() => {
         container.style.bottom = "0px";
         setTimeout(() => {
           container.style.bottom = "-29px";
-        }, 2500);
-      }
-    });
-    window.addEventListener('offline',()=>{
-      setIsOnline(false);
-      if(container !== null){
-        container.style.bottom = "0px";
-        setTimeout(() => {
-          container.style.bottom = "-29px";
-        }, 2500);
-      }
-    });
+          setTimeout(() => {
+            container.style.display = "none";
+          }, 200);
+        }, 2000);
+      }, 200);
+    }
+  }
+  useEffect(()=>{
+    window.addEventListener('online',()=>{showSign(true)});
+    window.addEventListener('offline',()=>{showSign(false)});
   },[])
   return (
     <Container style={{backgroundColor:(isOnline ? "#2fb67e" : "#e01e5a")}} id={Container.styledComponentId} isOnline={String(isOnline)}>
@@ -34,8 +33,8 @@ const NetworkSign = () => {
 const Container = styled.div<{isOnline:string}>`
   width:100%;
   position: fixed;
+  display:none;
   bottom: -29px;
-  display:flex;
   justify-content: center;
   align-items: center;
   transition: 0.2s;
